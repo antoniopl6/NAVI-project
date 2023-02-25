bcfe921d1c44daa06d3db7bca34a28d98482e022
antoniopl6 <72573790+antoniopl6@users.noreply.github.com>
2023-02-25 11:59:46 +0100

    new player script
    movement fixed

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody2D myRB;
+
+    [SerializeField]
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
+
+    }
+}
